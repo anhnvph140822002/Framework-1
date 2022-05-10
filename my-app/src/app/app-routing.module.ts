b8d695af57e32fbf2d4af4845730739fc0ff22dd
Viet Anh Ngo <ngovietanh503@gmail.com>
2022-05-10 13:55:30 +0700

    feat:Demo hồi sáng

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AboutComponent } from './components/about/about.component';
+import { ProductsComponent } from './components/products/products.component';
+const routes: Routes = [
+  {path:"product", component: ProductsComponent},
+  {path:"about", component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
